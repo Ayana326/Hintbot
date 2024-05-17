e01b38394be22bfecda8857a287a5a46c17e2683
chest760 <chestnut0725@gmail.com>
2024-05-18 01:54:13 +0900

    add code editor and chat

--- frontend/src/components/editor/CodeEditor.tsx
+++ frontend/src/components/editor/CodeEditor.tsx
@@ -1,4 +1,5 @@
 import { Editor } from "@monaco-editor/react";
+import { Button } from "@mui/material";
 
 const DefaultCode: string = `import sys
 
@@ -11,37 +12,34 @@ if __name__ == "__main__":
 `;
 
 export const IDE = () => {
-  const sleep = (ms: any) => new Promise((resolve) => setTimeout(resolve, ms));
-
   const handleSubmit = async () => {
     console.log("3秒後に表示");
   };
   return (
     <div className="flex items-start flex-col">
       <div className="w-full p-2  border">
-        <form action="#" onSubmit={handleSubmit}>
-          <div className="">
-            <label htmlFor="comment" className="sr-only">
-              Add your code
-            </label>
-            <Editor
-              height="40vh"
-              defaultLanguage="python"
-              defaultValue={DefaultCode}
-            />
+        <div className="">
+          <label htmlFor="comment" className="sr-only">
+            Add your code
+          </label>
+          <Editor
+            height="40vh"
+            defaultLanguage="python"
+            defaultValue={DefaultCode}
+          />
+        </div>
+        <div className="flex justify-between pt-2">
+          <div className="flex items-center space-x-5"></div>
+          <div className="flex-shrink-0">
+            <Button
+              onClick={() => handleSubmit()}
+              variant="outlined"
+              color="error"
+            >
+              提出
+            </Button>
           </div>
-          <div className="flex justify-between pt-2">
-            <div className="flex items-center space-x-5"></div>
-            <div className="flex-shrink-0">
-              <button
-                type="submit"
-                className="inline-flex items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white hover:bg-indigo-500"
-              >
-                提出
-              </button>
-            </div>
-          </div>
-        </form>
+        </div>
       </div>
     </div>
   );
